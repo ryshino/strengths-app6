6b7d7fe9587a86c5d6037a603b85bf85d4514119
ryshino <ranature1153@gmail.com>
2022-10-23 10:41:33 +0900

    ユーザー登録機能を実装

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "ユーザー登録に成功しました"
+      redirect_to user_path(@user)
+    else
+      flash.now[:alret] = "入力情報に誤りがあります"
+      render 'new'
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to new_user_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :profile, :password, :password_confirmation)
+    end
+end
