c80eb64366049ab74b5b8fe96eaae330bb7cf3d2
ryshino <ranature1153@gmail.com>
2022-10-25 07:28:38 +0900

    ログイン機能を実装

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(profile: params[:session][:profile])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user_path(user)
+    else
+      flash.now[:alret] = "入力情報に誤りがあります"
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url
+  end
+end
