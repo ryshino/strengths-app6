eee7af7b9b3e64c3fe6b6f4a86cd550facafd15f
ryshino <ranature1153@gmail.com>
2022-10-28 07:45:17 +0900

    永続ログイン機能を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(profile: params[:session][:profile])
     if user && user.authenticate(params[:session][:password])
       log_in(user)
+      remember(user)
       redirect_to user_path(user)
     else
       flash.now[:alret] = "入力情報に誤りがあります"
@@ -16,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
